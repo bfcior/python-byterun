1ac543e4be9d1f4414d9288f657f05abb3de3a1d
Piotr Wasilewski <piotr.wasyl@gmail.com>
2017-06-08 15:17:26 +0200

    Add byterun class

---
+++ byterun.py
@@ -0,0 +1,35 @@
+class Byterun:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def hex_string_decode(hex_string):
+
+        arr = []
+
+        i = 0
+        check = False
+        last = None
+
+        for char in hex_string:
+
+            if check:
+                check = False
+                continue
+
+            arr.append(char)
+
+            if char == last:
+                check = True
+                l = ord(hex_string[i + 1])
+                for j in xrange(0, l):
+                    arr.append(char)
+
+                i += 2
+                continue
+
+            last = char
+            i += 1
+
+        return arr
